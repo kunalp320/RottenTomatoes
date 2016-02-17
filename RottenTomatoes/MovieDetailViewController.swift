bbcf0cfe1aae5277c2bbe52e5514b7c5ae5608bd
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-17 14:14:25 -0800

    Incorporated configuration file

--- RottenTomatoes/MovieDetailViewController.swift
+++ RottenTomatoes/MovieDetailViewController.swift
@@ -15,15 +15,15 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var movieOverviewLabel: UILabel!
     @IBOutlet weak var movieTitleLabel: UILabel!
-    let baseImageUrl = "http://image.tmdb.org/t/p/w500"
     var movie: NSDictionary!
+    var configurations:APIConfiguration = APIConfiguration()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.movieOverviewLabel.text = movie["overview"] as? String
         self.movieTitleLabel.text = movie["title"] as? String
         
-        let imageUrl = NSURL(string: self.baseImageUrl + (movie["poster_path"] as! String))
+        let imageUrl = NSURL(string: configurations.baseImageUrl + (movie["poster_path"] as! String))
         posterImageView.setImageWithURL(imageUrl!)
         
     }
